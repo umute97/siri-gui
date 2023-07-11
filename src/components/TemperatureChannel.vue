c8f79f2843eaebdf9d6c0dcd6678541365a245f0
Umut Elicabuk <umut.elicabuk@cern.ch>
2023-07-11 14:56:21 +0200

    Make temperature channel highlightable

--- src/components/TemperatureChannel.vue
+++ src/components/TemperatureChannel.vue
@@ -1,13 +1,13 @@
 <template>
-    <section class="temperature-channel">
-        <h1 class="channel">Ch. {{ props.channelNumber }}</h1>
-        <div class="temperature">{{ currentTemperature }} °C</div>
+    <section class="temperature-channel" @click="toggleHighlight" :class="highlightedClass">
+        <h1 class="channel" :class="highlightedTextClass">Ch. {{ props.channelNumber }}</h1>
+        <div class="temperature" :class="highlightedTextClass">{{ currentTemperature }} °C</div>
     </section>
 </template>
 <script setup lang="ts">
 import { useTemperatureStore } from '@/stores/stores';
 import { storeToRefs } from 'pinia';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const temperatureStore = useTemperatureStore();
 const { temperatures } = storeToRefs(temperatureStore);
@@ -22,6 +22,23 @@ const props = defineProps({
         required: true
     },
 })
+
+const highlighted = ref(false);
+
+const highlightedClass = computed(() => {
+    if (highlighted.value) return 'highlighted';
+    return '';
+})
+
+const highlightedTextClass = computed(() => {
+    if (highlighted.value) return 'highlighted-text';
+    return '';
+})
+
+const toggleHighlight = () => {
+    highlighted.value = !highlighted.value;
+}
+
 </script>
 <style scoped>
 .temperature-channel {
@@ -39,5 +56,13 @@ const props = defineProps({
     font-weight: bold;
     color: var(--primary-color);
 }
-    
+
+.highlighted-text {
+    color: var(--zinc);
+}
+
+.highlighted {
+    background: var(--primary-color);
+    color: var(--zinc);
+}
 </style>
